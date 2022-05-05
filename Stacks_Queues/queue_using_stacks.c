f507156cc0eef694df360a8d2dcb4a31936bb239
Yashas B K <yashas.bk@thundersoft.com>
2022-05-05 09:10:56 +0530

    Enqueue operation completed, dequeue has some errors

--- Stacks_Queues/queue_using_stacks.c
+++ Stacks_Queues/queue_using_stacks.c
@@ -4,5 +4,72 @@
 
 
 #include <stdio.h>
+#include<stdlib.h>
 
-int main();
+typedef struct node{
+	int data;
+	struct node *link;
+}stack1,stack2;
+
+stack1 *top1 = NULL;
+stack2 *top2 = NULL;
+
+void enqueue(int data);
+void dequeue();
+
+int main()
+{
+	enqueue(1);
+	enqueue(2);
+	enqueue(3);
+	dequeue();
+}
+
+void enqueue(int data)
+{
+	stack1 *newnode1 = (stack1*)malloc(sizeof(stack1));
+	if(newnode1 == NULL)
+	{
+		printf("Memory not allocated\n");
+	}
+	else
+	{
+		newnode1->data = data;
+		newnode1->link = top1;
+		printf("Inserted element is %d\n",newnode1->data);
+		top1 = newnode1;
+	}
+}
+
+void dequeue()
+{
+	if(top1 == NULL)
+	{
+		printf("There are no elements in queue\n");
+	}
+	else
+	{
+		stack2 *newnode2 = (stack2*)malloc(sizeof(stack2));
+		if(newnode2 == NULL)
+		{
+			printf("Memory not allocated\n");
+		}
+		else
+		{
+			stack1 *temp = NULL;
+			while(temp->link != NULL)
+			{
+				temp = top1;
+				top1 = temp->link;
+				newnode2->link = top2;
+				newnode2->data = temp->data;
+				top2 = newnode2;
+				free(temp);
+			}
+			stack2 *temp_free = top2;
+			top2 = temp_free->link;
+			printf("dequeued element is %d\n",temp_free->data);
+			free(temp_free);
+		}
+	}
+}
