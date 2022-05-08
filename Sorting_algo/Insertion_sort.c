f536c459b937121f84c36a000fa82f7dc186a37d
Yashas B K <yashas.bk@thundersoft.com>
2022-05-08 17:55:31 +0530

    Added insertion sort

---
+++ Sorting_algo/Insertion_sort.c
@@ -0,0 +1,47 @@
+// Bubble sort
+
+#include <stdio.h>
+
+void insertion_sort(int row, int *arr)
+{
+    int temp = 0,i,j;
+    for (i = 1; i < row; i++)
+    {
+        temp = arr[i];
+        j = i-1;
+        while(j>=0 && arr[i]>temp)
+        {
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = temp;
+    }
+}
+
+void print_arr(int len, int *arr)
+{
+    for (int i = 0; i < len; i++)
+    {
+        printf("%d ", *(arr + i));
+    }
+    putchar('\n');
+}
+
+int main()
+{
+    int len;
+    printf("Enter the length of array:");
+    scanf("%d", &len);
+    int arr[len];
+    for (register int i = 0; i < len; i++)
+    {
+        printf("Enter the element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Before sorting:\n");
+    print_arr(sizeof(arr) / sizeof(int), arr);
+    insertion_sort(sizeof(arr) / sizeof(int), arr);
+    printf("After sorting:\n");
+    print_arr(sizeof(arr) / sizeof(int), arr);
+}
